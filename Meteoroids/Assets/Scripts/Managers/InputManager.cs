134092ad8df51fd2db02affd6274e3d4b7048dc0
Marc <marcmartin315@gmail.com>
2025-06-16 13:25:20 +0200

    Created InputManager script for detecting and broadcasting the actions performed by the player.

---
+++ Meteoroids/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance;
+
+    public event EventHandler<float> OnPlayerRotates;
+    public event EventHandler OnPlayerThrusts;
+    public event EventHandler OnPlayerShoots;
+    public event EventHandler OnPlayerTeleports;
+    public event EventHandler OnPlayerUsesPowerUp;
+
+    [SerializeField] private InputActionReference _rotate;
+    [SerializeField] private InputActionReference _thrust;
+    [SerializeField] private InputActionReference _shoot;
+    [SerializeField] private InputActionReference _teleport;
+    [SerializeField] private InputActionReference _usePowerUp;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        InitActions();
+        EnableInGameActions();
+    }
+
+    private void InitActions()
+    {
+        InitRotateAction();
+        InitThrustAction();
+        InitShootAction();
+        InitTeleportAction();
+        InitUsePowerUpAction();
+    }
+    private void EnableInGameActions()
+    {
+        _rotate.action.Enable();
+        _thrust.action.Enable();
+    }
+    private void DisableInGameActions()
+    {
+        _rotate.action.Disable();
+        _thrust.action.Disable();
+    }
+
+    #region RotateAction
+    private void InitRotateAction() => _rotate.action.performed += RotatePerformed;
+    private void RotatePerformed(InputAction.CallbackContext obj) => OnPlayerRotates?.Invoke(this, obj.ReadValue<float>());
+    #endregion
+
+    #region ThrustAction
+    private void InitThrustAction() => _thrust.action.performed += ThrustPerformed;
+    private void ThrustPerformed(InputAction.CallbackContext obj) => OnPlayerThrusts?.Invoke(this, EventArgs.Empty);
+    #endregion
+
+    #region ShootAction
+    private void InitShootAction() => _shoot.action.performed += ShootPerformed;
+    private void ShootPerformed(InputAction.CallbackContext obj) => OnPlayerShoots?.Invoke(this, EventArgs.Empty);
+    #endregion
+
+    #region TeleportAction
+    private void InitTeleportAction() => _shoot.action.performed += TeleportPerformed;
+    private void TeleportPerformed(InputAction.CallbackContext obj) => OnPlayerTeleports?.Invoke(this, EventArgs.Empty);
+    #endregion
+
+    #region UsePowerUpAction
+    private void InitUsePowerUpAction() => _shoot.action.performed += UsePowerUpPerformed;
+    private void UsePowerUpPerformed(InputAction.CallbackContext obj) => OnPlayerUsesPowerUp?.Invoke(this, EventArgs.Empty);
+    #endregion
+}
