0ac1db0e50e8f6a662810d14946807be6dd5125d
Marc <marcmartin315@gmail.com>
2025-06-20 17:18:32 +0200

    GameBootstrapper now initializes AsteroidFactory.

--- Meteoroids/Assets/Scripts/Utils/GameBootstrapper.cs
+++ Meteoroids/Assets/Scripts/Utils/GameBootstrapper.cs
@@ -1,12 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameBootstrapper : MonoBehaviour
 {
     [SerializeField] private AudioLibrary _audioLibrary;
+    [SerializeField] private List<AsteroidConfig> _asteroidConfigurations;
 
     private void Awake()
     {
         AudioManager.Init(_audioLibrary);
+        AsteroidFactory.Init(_asteroidConfigurations);
 
         DontDestroyOnLoad(gameObject);
     }
